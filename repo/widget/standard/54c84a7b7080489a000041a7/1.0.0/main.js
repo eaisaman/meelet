f07a1cf99216dd435766c7f4139f4bfa1dfc4455
jill <eaisaman@hotmail.com>
2015-01-30 15:41:30 +0800

    Add new repo widget.

---
+++ repo/widget/standard/54c84a7b7080489a000041a7/1.0.0/main.js
@@ -0,0 +1,37 @@
+define(
+    [],
+    function () {
+
+        return {
+            js: ["directive/ng.ui.widget.tab"],
+            stylesheets: ["stylesheets/widget.css"],
+            demo: {
+                js: ["demo/demo.js"],
+                stylesheets: ["stylesheets/demo.css"],
+                url: "demo/tabDemo.html"
+            },
+            template: "app/widget.html",
+            configuration: {
+                tabTitles: {
+                    name: "Tabs",
+                    type: "writableList"
+                },
+                pickedTabTitle: {
+                    name: "Active Tab",
+                    type: "readableList",
+                    "listName": "tabTitles"
+                },
+                align: {
+                    name: "Align",
+                    type: "list",
+                    options: [
+                        {name: "alignTop", value: "alignTop"},
+                        {name: "alignBottom", value: "alignBottom"},
+                        {name: "alignLeft", value: "alignLeft"},
+                        {name: "alignRight", value: "alignRight"}
+                    ]
+                }
+            }
+        };
+    }
+);
