28a5573129763597b24358f83476ee215d5c8030
jill <eaisaman@hotmail.com>
2015-04-30 14:11:45 +0800

    Bug Fix.

--- public/javascripts/ckeditor/4.4.5/main.js
+++ public/javascripts/ckeditor/4.4.5/main.js
@@ -9,10 +9,38 @@ requirejs.config(
 
 define(
     [
-            "ckeditor"
+        "ckeditor"
     ],
     function () {
         CKEDITOR.disableAutoInline = true;
+
+        CKEDITOR.config.toolbar_Meelet = [
+
+            ['Cut','Copy','Paste','PasteText','PasteFromWord','-', 'SpellChecker', 'Scayt'],
+
+            ['Undo','Redo','-','Find','Replace','-','SelectAll','RemoveFormat'],
+
+            ['Form', 'Checkbox', 'Radio', 'TextField', 'Textarea', 'Select'],
+
+            '/',
+
+            ['Bold','Italic','Underline','Strike','-','Subscript','Superscript'],
+
+            ['NumberedList','BulletedList','-','Outdent','Indent'],
+
+            ['JustifyLeft','JustifyCenter','JustifyRight','JustifyBlock'],
+
+            ['Image','Table','HorizontalRule','Smiley','SpecialChar','PageBreak'],
+
+            '/',
+
+            ['Styles','Format','Font','FontSize'],
+
+            ['TextColor','BGColor']
+
+        ];
+
+        CKEDITOR.config.toolbar = "Meelet";
 
         return function () {
         }
--- public/javascripts/ckeditor/4.4.7/main.js
+++ public/javascripts/ckeditor/4.4.7/main.js
@@ -14,6 +14,34 @@ define(
     function () {
         CKEDITOR.disableAutoInline = true;
 
+        CKEDITOR.config.toolbar_Meelet = [
+
+            ['Cut','Copy','Paste','PasteText','PasteFromWord','-', 'SpellChecker', 'Scayt'],
+
+            ['Undo','Redo','-','Find','Replace','-','SelectAll','RemoveFormat'],
+
+            ['Form', 'Checkbox', 'Radio', 'TextField', 'Textarea', 'Select'],
+
+            '/',
+
+            ['Bold','Italic','Underline','Strike','-','Subscript','Superscript'],
+
+            ['NumberedList','BulletedList','-','Outdent','Indent'],
+
+            ['JustifyLeft','JustifyCenter','JustifyRight','JustifyBlock'],
+
+            ['Image','Table','HorizontalRule','Smiley','SpecialChar','PageBreak'],
+
+            '/',
+
+            ['Styles','Format','Font','FontSize'],
+
+            ['TextColor','BGColor']
+
+        ];
+
+        CKEDITOR.config.toolbar = "Meelet";
+
         return function () {
         }
     }
